aa1e0ed15585c611bb6e132fd94e969411849e85
shixiaomingya <62506713+shixiaomingya@users.noreply.github.com>
2023-04-21 12:04:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -32,20 +32,14 @@ const USER_CONFIG = {
         {
           type: '*生日', name: '宝贝', year: '1997', date: '12-05',
         },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2023', date: '03-29',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2023-04-07' },
       ],
     },
   ],
